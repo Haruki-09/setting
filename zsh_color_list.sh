20e4901a4e63503cbabb170c3f353a0494d7d929
haruki(wsl2) <haruki@wsl2>
2022-12-25 11:18:01 +0900

    new zsh color list script

---
+++ zsh_color_list.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+colorlist() {
+	for color in {000..015}; do
+		print -nP "%F{$color}$color %f"
+	done
+	printf "\n"
+	for color in {016..255}; do
+		print -nP "%F{$color}$color %f"
+		if [ $(($((color-16))%6)) -eq 5 ]; then
+			printf "\n"
+		fi
+	done
+}
+
+colorlist
